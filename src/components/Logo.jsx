c914203576c0f21f18592a69a544b28cf5893da6
Kanchan Dixit <kanchan.dixit2023@gmail.com>
2024-12-24 04:06:55 +0530

    updating path of images

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@
 function Logo() {
   return (
     <div className='flex items-center'>
-      <img src='public\images\logo.svg' alt='PostWriter Logo' className='w-8 h-8 mr-2' />
+      <img src='/public/images/logo.svg' alt='PostWriter Logo' className='w-8 h-8 mr-2' />
       <h1 className='text-2xl font-semibold text-blue-800'>PostWriter</h1>
     </div>
   )
